b2b2fc62189b9b1a8500e299a1743840765e294a
Giulio Nisi <email@giulionisi.it>
2024-12-02 20:20:04 +0100

    Fixed Unused GSON Warn

--- CROSS/src/Main.java
+++ CROSS/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Main 42!");
         Gson gson = new Gson();
+        gson.toJson("Hello", System.out);
     }
 }
