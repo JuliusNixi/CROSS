29da18f5885baaa9ac57eda64792171b35cea20c
Giulio Nisi <email@giulionisi.it>
2025-03-01 18:30:13 +0100

    Major changes. Various reviews. Added persistent database orders file logs.

---
+++ CROSS/src/CROSS/Exceptions/InvalidOrder.java
@@ -0,0 +1,29 @@
+package CROSS.Exceptions;
+
+/**
+ * 
+ * Exception for invalid order. 
+ * It's throwed, for example, when adding an order that already exists in the orders database.
+ * 
+ * @version 1.0
+ * @author Giulio Nisi
+ * 
+ * @see Exception
+ * 
+ */
+public class InvalidOrder extends Exception {
+
+    /**
+     * 
+     * Constructor of the exception.
+     * 
+     * @param message Message of the exception.
+     * 
+     */
+    public InvalidOrder(String message) {
+
+        super(message);
+        
+    }
+
+}
